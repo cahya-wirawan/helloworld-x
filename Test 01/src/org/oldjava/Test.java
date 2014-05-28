11d4cdc77de6b0359ac28465882651802b8dd108
Cahya <cahya@mail.com>
2014-05-28 15:56:51 +0200

    add 3.line

--- Test 01/src/org/oldjava/Test.java
+++ Test 01/src/org/oldjava/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test 01");
 		System.out.println("Second line.");
-
+		System.out.println("Third line.");
 	}
 
 }
